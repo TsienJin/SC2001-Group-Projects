55d9e5a544f59acbde66ecb1b32c86c465d98a6e
Wei Kang <76279908+kang5647@users.noreply.github.com>
2022-09-08 00:17:48 +0800

    Add files via upload

---
+++ Proj1/sort/mergeSort2.py
@@ -0,0 +1,54 @@
+
+class MergeSort: 
+
+    def __init__(self, array) -> None:
+       self.name = "Merge Sort"
+       self.comparison = 0
+       self.swaps = 0
+       self.array = array
+       self.sort(array)
+       
+
+    def sort(self, arr) -> None:
+
+     if len(arr) > 1: 
+
+        #Findin the mid of the array
+        mid = len(arr) //2 
+
+        #Dividing the array into 2 halves 
+        L = arr[:mid]
+
+        R = arr[mid:]
+
+        #Sorting the first half
+        self.sort(L)
+
+        #Sorting the second half 
+        self.sort(R)
+
+        i = j = k = 0
+        
+        #using temp array L and R, sort the elements back to arr
+        while i<len(L) and j < len(R):
+            if L[i] < R[j]:
+               arr[k] = L[i]
+               i += 1
+            
+            else: 
+                arr[k] = R[j]
+                j += 1
+                
+            k += 1
+           # self.comparison +=1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i +=1
+            k +=1 
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
