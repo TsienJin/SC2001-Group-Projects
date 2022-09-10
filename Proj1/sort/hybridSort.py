ede35a630f2841c2c0b500ca2677549a71973468
Wei Kang <76279908+kang5647@users.noreply.github.com>
2022-09-10 20:33:08 +0800

    Add files via upload

---
+++ Proj1/sort/hybridSort.py
@@ -0,0 +1,73 @@
+class HybridSort: 
+
+    def __init__(self, array) -> None:
+       self.name = "Merge _sort"
+       self.comparison = 0
+       self.swaps = 0
+       self.array = array
+       self.threshold = 2  #set threshold to observe changes in time complexity and key comparisons
+
+    #self-calling method
+    def sort(self) -> None:
+        self._sort(self.array)
+
+    #insertionSort method 
+    def insertSort(self, array)-> None:
+        arrayNum = len(array);     #Count number of elements in array
+        if(arrayNum >= 1):              #Check if more than 1 element exists
+            for i in range(1, arrayNum, 1):
+                for j in range(i, 0, -1):
+                    self.comparison += 1
+                    if(array[j] < array[j-1]):    #Compare if left side element is greater than the right side element
+                        self.swap(array, j, j-1)       #Swap the elements
+                        self.swaps += 1
+
+    #function for swapping 2 elements 
+    def swap(array, pos1, pos2) -> None:
+        array[pos1], array[pos2] = array[pos2], array[pos1] #Swap 2 nodes
+
+    #Hybrid sort (insertSort + mergeSort)
+    def _sort(self, array) -> None:
+        #if arraysize < threshold, use insertion sort method 
+        if len(array) <= self.threshold: 
+            self.insertSort(array)
+    
+        else:
+            #Findin the mid of the array
+            mid = len(array) //2 
+
+            #Dividing the array into 2 halves 
+            L = array[:mid]
+
+            R = array[mid:]
+
+             #Sorting the first half
+            self._sort(L)
+
+             #Sorting the second half 
+            self._sort(R)
+
+            i = j = k = 0
+        
+            #using temp array L and R, _sort the elements back to arr
+            while i<len(L) and j < len(R):
+                if L[i] < R[j]:
+                   array[k] = L[i]
+                   i += 1
+            
+                else: 
+                    array[k] = R[j]
+                    j += 1
+                
+                k += 1
+                self.comparison +=1
+
+            while i < len(L):
+                array[k] = L[i]
+                i +=1
+                k +=1 
+
+            while j < len(R):
+                array[k] = R[j]
+                j += 1
+                k += 1
