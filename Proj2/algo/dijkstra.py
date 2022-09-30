98cfb93f54325e2313f84731e7c26f01fe08ec6f
Tsien Jin <tsien.jin@gmail.com>
2022-09-30 21:48:03 +0800

    House keeping

--- Proj2/algo/dijkstra.py
+++ Proj2/algo/dijkstra.py
@@ -8,18 +8,19 @@ class DSearch():
     def __init__(self, graph:GenerateTest, useHeap:bool=False, startNode:int=0) -> None:
         self.graph = graph
         self.adjMatrix = self.graph.adjMatrix
+        self.adjArr = self.adjArr
         self.useHeap = useHeap
         self.startNode = startNode
         
         
-        self.distNode = []
+        self.distFromStart = [] # i'th index is the distance of i'th node from start node
         self.predecessor = []
         self.solutionSet = []
         
-        # shortest path tree (SPT) set,
+        # shortest path tree (SPT) set = self.visited
         # keeps track of vertices that are included into the SPT
         # when the values are finalised
-        self.sptSet = [False] * self.graph.dimension
+        self.visited = [False] * self.graph.dimension
         
         
         if(useHeap):
@@ -33,42 +34,47 @@ class DSearch():
         
         
     def solve(self) -> bool:
-        # returns true if the graph is solvable
-        # returns false if the graph is NOT solvable
+        if(self.useHeap):
+            self.__dHeap()
+        else:
+            self.__dArray()
         pass
     
-    def __shortestDist(self, priorityQueue) -> int:
+    def __dHeap(self) -> None:
         pass
     
-    def dijkstra2(self) -> None:
-        self.distNode = [1e7] * self.graph.dimension
-        self.predecessor = [-1] * self.graph.dimension
-        # self.sptSet = [False] * self.graph.dimension
-        
-        # insert first element into the queue
-        self.queue.insert()
+    def __dArray(self) -> None:
+        pass
     
-    def dijkstra1(self) -> None:
-        for i in range(len(self.adjMatrix)):
-            self.distNode[i] = np.inf
-            self.predecessor[i] = -1
-            self.solutionSet[i] = 0
-        self.solutionSet[self.startNode] = 1
-        self.distNode[self.startNode] = 0
+    # def dijkstra2(self) -> None:
+    #     self.distNode = [1e7] * self.graph.dimension
+    #     self.predecessor = [-1] * self.graph.dimension
+    #     # self.visited = [False] * self.graph.dimension
         
-        # Queue stuff here
+    #     # insert first element into the queue
+    #     self.queue.insert()
+    
+    # def dijkstra1(self) -> None:
+    #     for i in range(len(self.adjMatrix)):
+    #         self.distNode[i] = np.inf
+    #         self.predecessor[i] = -1
+    #         self.solutionSet[i] = 0
+    #     self.solutionSet[self.startNode] = 1
+    #     self.distNode[self.startNode] = 0
         
-        while(len(self.tempQueue) != 0):
-            shortestNode = self.shortestDist(self.tempQueue)
-            self.solutionSet[shortestNode] = 1
+    #     # Queue stuff here
+        
+    #     while(len(self.tempQueue) != 0):
+    #         shortestNode = self.shortestDist(self.tempQueue)
+    #         self.solutionSet[shortestNode] = 1
             
-            #BFS HERE
+    #         #BFS HERE
             
-            for i in range(len(self.bfsQueue)):
-                if(self.predecessor[i] != 1 and self.distNode[i] > self.distNode[shortestNode] + self.adjMatrix[i][shortestNode]):
-                    self.bfsQueue.pop()
-                    self.distNode[i] = self.distNode[shortestNode] + self.adjMatrix[i][shortestNode]
-                    self.predecessor[i] = shortestNode
-                    # Put i into priority queue
-                pass
+    #         for i in range(len(self.bfsQueue)):
+    #             if(self.predecessor[i] != 1 and self.distNode[i] > self.distNode[shortestNode] + self.adjMatrix[i][shortestNode]):
+    #                 self.bfsQueue.pop()
+    #                 self.distNode[i] = self.distNode[shortestNode] + self.adjMatrix[i][shortestNode]
+    #                 self.predecessor[i] = shortestNode
+    #                 # Put i into priority queue
+    #             pass
     
