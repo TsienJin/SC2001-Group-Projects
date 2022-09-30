d1e6d30eda06b5774f884ee59586fee75dd60e44
Tsien Jin <tsien.jin@gmail.com>
2022-09-30 21:48:10 +0800

    Update main.py

--- Proj2/main.py
+++ Proj2/main.py
@@ -2,27 +2,23 @@ from test.generateTest import GenerateTest
 from algo.ArrayQueue import ArrayQueue
 
 def main():
-    obj = GenerateTest(dimension=10, isDirectional=True)
-    print(str(obj))
-    print(repr(obj))
-    # obj.printBoth()
+    obj = GenerateTest(dimension=10, isDirectional=False)
+    # print(str(obj))
+    # print(repr(obj))
+    obj.printBoth()
     
     # #create queue object
     queue = ArrayQueue()
 
     # #get adjList
     adjList = obj.getAdjList()
-    #insert the edges of the 2nd node
-    for i in adjList[1]:
-        temp = i
-        print(i)
-        queue.insert(temp)
+    for srcNode in adjList.keys():
+        for edge in adjList[srcNode]:
+            queue.insert(sourceNode=srcNode, destNode=edge[0], weight=edge[1])
     
-    # print(queue)
-    # #pop the node with the least weight
-    # item = queue.pop()
-    # print(item)
-    # print(queue)
+    print(queue)
+    print("Lowest node with source 0:")
+    print(queue.popFrom(source=0))
     
 if __name__ == '__main__':
     main()
