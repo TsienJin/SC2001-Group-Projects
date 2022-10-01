798a7142809527db0dc20628e7b3dc6b80eb1d87
Tsien Jin <tsien.jin@gmail.com>
2022-10-01 18:56:31 +0800

    Create HeapQueue.py

---
+++ Proj2/algo/HeapQueue.py
@@ -0,0 +1,18 @@
+from algo.Queue import QueueEdge
+
+class HeapQueue():
+    def __init__(self):
+        self.queue = []
+        
+    def __str__(self):
+        return '\n'.join([str(i) for i in self.queue])
+    
+    def isEmpty(self):
+        return len(self.queue) == 0
+    
+    def insert(self, sourceNode:int, destNode:int, weight:int=0) -> None:
+        self.queue.append()
+        
+    def pop(self) -> QueueEdge:
+        pass
+    
