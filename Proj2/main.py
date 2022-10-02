56e4143ac9941a96196bdc7461a8c08976a03eb1
Tsien Jin <tsien.jin@gmail.com>
2022-10-02 08:20:15 +0800

    Update main.py

--- Proj2/main.py
+++ Proj2/main.py
@@ -2,36 +2,23 @@ from algo.dijkstra import DSearch
 from test.generateTest import GenerateTest
 from algo.ArrayQueue import ArrayQueue
 
-def main():
-    obj = GenerateTest(dimension=5, isDirectional=False)
-    # print(str(obj))
-    # print(repr(obj))
+def debug():
+    obj = GenerateTest(dimension=5, isDirectional=True)
     obj.printBoth()
-    
-    # # #create queue object
-    # queue = ArrayQueue()
-
-    # # #get adjList
-    # adjList = obj.getAdjList()
-    # for srcNode in adjList.keys():
-    #     for edge in adjList[srcNode]:
-    #         queue.insert(sourceNode=srcNode, destNode=edge[0], weight=edge[1])
-    
-    # print(queue)
-    # print("Lowest node with source 0:")
-    # print(queue.popFrom(source=0))
     
     print("\nArray")
     print("+"*10)
-    dijk = DSearch(graph=obj, useHeapAndList=True)
+    dijk = DSearch(graph=obj, useHeapAndList=False)
     dijk.solve()
     dijk.printMST()
     
     print("\nHeap")
     print("+"*10)
-    dijk1 = DSearch(graph=obj, useHeapAndList=False)
+    dijk1 = DSearch(graph=obj, useHeapAndList=True)
     dijk1.solve()
     dijk1.printMST()
     
+
+    
 if __name__ == '__main__':
-    main()
+    debug()
