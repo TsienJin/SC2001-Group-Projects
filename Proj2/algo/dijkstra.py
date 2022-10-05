672e98224fa56a7bab0ec57d8cc7f72e413db64b
Tsien Jin <tsien.jin@gmail.com>
2022-10-05 14:09:12 +0800

    Changes

--- Proj2/algo/dijkstra.py
+++ Proj2/algo/dijkstra.py
@@ -30,10 +30,7 @@ class DSearch():
     ### PUBLIC CALLABLE METHODS
         
     def solve(self) -> bool:
-        if(self.useHeapAndList):
-            self.__dHeap()
-        else:
-            self.__dArray()
+        self.__dijkstra()
 
     
     
@@ -69,16 +66,13 @@ class DSearch():
     
     
     ### Dijkstra methods
-    
-    def __dHeap(self) -> None:
+    def __dijkstra(self) -> None:
         # init arrays for starting node
         self.distFromStart[self.startNode] = 0
         self.visited[self.startNode] = True
         
         # add starting node to the queue
-        curNode = self.startNode
-        self.__insertAdjNodesToQueueFrom(curNode)
-        
+        self.__insertAdjNodesToQueueFrom(self.startNode)
         
         while not self.queue.isEmpty():
             nextEdge = self.queue.pop()
@@ -87,64 +81,3 @@ class DSearch():
                 self.distFromStart[nextEdge.destNode] = self.distFromStart[nextEdge.sourceNode] + nextEdge.weight
                 self.path.append(nextEdge.destNode)
                 self.__insertAdjNodesToQueueFrom(nextEdge.destNode)
-    
-    def __dArray(self) -> None:
-        # init arrays for starting node
-        self.distFromStart[self.startNode] = 0
-        self.visited[self.startNode] = True
-        
-        # add starting node to the queue
-        curNode = self.startNode
-        self.__insertAdjNodesToQueueFrom(curNode)
-        
-        
-        while not self.queue.isEmpty():
-            nextEdge = self.queue.pop()
-            self.visited[nextEdge.sourceNode] = True
-            if(self.visited[nextEdge.destNode] == False):
-                self.distFromStart[nextEdge.destNode] = self.distFromStart[nextEdge.sourceNode] + nextEdge.weight
-                self.path.append(nextEdge.destNode)
-                self.__insertAdjNodesToQueueFrom(nextEdge.destNode)
-                
-        
-                
-            
-            
-            
-        
-        
-        
-        
-    
-    # def dijkstra2(self) -> None:
-    #     self.distNode = [1e7] * self.graph.dimension
-    #     self.predecessor = [-1] * self.graph.dimension
-    #     # self.visited = [False] * self.graph.dimension
-        
-    #     # insert first element into the queue
-    #     self.queue.insert()
-    
-    # def dijkstra1(self) -> None:
-    #     for i in range(len(self.adjMatrix)):
-    #         self.distNode[i] = np.inf
-    #         self.predecessor[i] = -1
-    #         self.solutionSet[i] = 0
-    #     self.solutionSet[self.startNode] = 1
-    #     self.distNode[self.startNode] = 0
-        
-    #     # Queue stuff here
-        
-    #     while(len(self.tempQueue) != 0):
-    #         shortestNode = self.shortestDist(self.tempQueue)
-    #         self.solutionSet[shortestNode] = 1
-            
-    #         #BFS HERE
-            
-    #         for i in range(len(self.bfsQueue)):
-    #             if(self.predecessor[i] != 1 and self.distNode[i] > self.distNode[shortestNode] + self.adjMatrix[i][shortestNode]):
-    #                 self.bfsQueue.pop()
-    #                 self.distNode[i] = self.distNode[shortestNode] + self.adjMatrix[i][shortestNode]
-    #                 self.predecessor[i] = shortestNode
-    #                 # Put i into priority queue
-    #             pass
-    
