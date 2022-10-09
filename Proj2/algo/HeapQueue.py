44139f88c3d0177bddcf0f6ef9fbd12f3dc25629
Wei Kang <76279908+kang5647@users.noreply.github.com>
2022-10-09 19:13:01 +0800

    Update HeapQueue.py
    Separate heapify to downheap and uphead: 
    upheap for rearranging priority when a new node is added 
    downheap for rearranging priority when a node is removed

--- Proj2/algo/HeapQueue.py
+++ Proj2/algo/HeapQueue.py
@@ -14,34 +14,42 @@ class HeapQueue():
 
     def insert(self, sourceNode:int, destNode:int, weight:int=0) -> None:
         self.queue.append(QueueEdge(sourceNode=sourceNode, destNode=destNode, weight=weight))
+        self.upHeap(len(self.queue)-1)
         
     def insertEdge(self, edge:QueueEdge):
         self.queue.append(edge)
        
     # HeapSort Functions
-    def heapify(self, arrLength, smallestDist) -> None:
-        rootPos = smallestDist # 2
-        leftPos = 2 * rootPos + 1
-        rightPos = 2 * rootPos + 2
-        currSmallest = rootPos
-        
-        if(leftPos < arrLength):
-            if(leftPos < arrLength) and (rightPos < arrLength):
-                currSmallest = self.smallestWt(leftPos, rightPos)
-                if(currSmallest == rightPos):
-                    self.queue[leftPos], self.queue[rightPos] = self.queue[rightPos], self.queue[leftPos]
-                    currSmallest = leftPos
-            currSmallest = self.smallestWt(currSmallest, rootPos)
-        
-        if(currSmallest != rootPos):
-            self.queue[rootPos], self.queue[currSmallest] = self.queue[currSmallest], self.queue[rootPos]
-            self.heapify(arrLength, currSmallest)
-            
-    def heapSort(self) -> None:
+    #upheap: when a new node is addded to the end of array, perform heapify, ie compare the new node with its parent, and swap places if necessary 
+    def upHeap(self, lastNode) -> None:
+        if(lastNode < 1): return 
+        parentNode = (lastNode -1) // 2
+
+        minNode = self.smallestWt(parentNode, lastNode)
+
+        if(minNode != parentNode):
+            self.queue[lastNode], self.queue[parentNode] = self.queue[parentNode], self.queue[lastNode]
+            self.upHeap(parentNode)
+    
+    #downheap: The node to be removed is the rootNode, pop() first swap the rootNode with the lastNode, the rootNode (which is now contains the lastNode) will do heapify 
+    #to check against its child node, and swap places if necessary 
+    def downHeap(self, parentNode) -> None:    
         arrLength = len(self.queue)
-        
-        for i in range(((arrLength // 2) - 1),-1 , -1):
-            self.heapify(arrLength, i)
+        leftNode = 2 * parentNode + 1 
+        rightNode = 2 * parentNode + 2 
+
+        if(leftNode < arrLength and rightNode< arrLength):
+            minNode = self.smallestWt(leftNode, rightNode)
+            minNode = self.smallestWt(minNode, parentNode)
+            if(minNode!= parentNode):
+                self.queue[minNode], self.queue[parentNode] = self.queue[parentNode], self.queue[minNode]
+                self.downHeap(minNode)
+
+        if(leftNode < arrLength):
+            minNode = self.smallestWt(leftNode, parentNode)
+            if(minNode!= parentNode):
+                self.queue[minNode], self.queue[parentNode] = self.queue[parentNode], self.queue[minNode]
+                self.downHeap(minNode)
     
     def smallestWt(self, first, second):
         return second if (self.queue[second].weight < self.queue[first].weight) else first
@@ -50,10 +58,13 @@ class HeapQueue():
     # regardless of source edge
     def pop(self) -> QueueEdge:
         if(not self.isEmpty()):
-            self.heapSort()
-            minEdge = self.queue[0] 
-            self.queue.remove(minEdge)
-            return minEdge
+            toPop = self.queue[0]
+            minNode = 0
+            lastNode = len(self.queue)-1
+            self.queue[minNode], self.queue[lastNode] = self.queue[lastNode],self.queue[minNode]
+            self.queue.remove(self.queue[lastNode])
+            self.downHeap(minNode)
+            return toPop
         else: 
             return None
         
@@ -62,6 +73,16 @@ class HeapQueue():
     def popFrom(self, source:int):
         
         minEdge = None
+        
+        if(not self.isEmpty()):
+            for edge in self.queue:
+                if(edge.sourceNode==source and (minEdge==None or edge.weight<minEdge.weight)):
+                    minEdge = edge
+            
+        if(minEdge != None):
+            self.queue.remove(minEdge)
+        
+        return minEdgeinEdge = None
         
         if(not self.isEmpty()):
             for edge in self.queue:
